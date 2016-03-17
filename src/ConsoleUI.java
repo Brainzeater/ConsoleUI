93841f5cea3a15d4111b1d8051783319bc515e32
Brainzeater <brainzeater@gmail.com>
2016-03-17 22:36:08 +0300

    Update

--- src/ConsoleUI.java
+++ src/ConsoleUI.java
@@ -7,22 +7,26 @@ import java.io.File;
 public class ConsoleUI {
     public static void main(String[] args){
         String RandomText = "";     //String to keep the content of RandomTextFile
-        String pair = "[A-Z]{1}[a-z0-9] +[a-z]{1}"; //String os pattern
+        String pairPiece = "[A-Z]{1}[a-z0-9]+[a-z]{1}"; //String os pattern
         /* Reading RandomTextFile and saving its content in RandomText */
         try{
             RandomText = new Scanner(new File("RandomTextFile.txt")).useDelimiter("\\Z").next();
         }catch(IOException ioe){
             ioe.printStackTrace();
         }
-        Pattern myPattern = Pattern.compile(pair);      //Pattern initialization
+        Pattern myPattern = Pattern.compile(pairPiece);      //Pattern initialization
         Matcher myMatcher = myPattern.matcher(RandomText);  //Matcher initialization
         int counterOfPairs = 0;     //Counter of pairs
+        int counterOfPieces = 0;
         /* Show matches */
         while(myMatcher.find()){
-            System.out.println("I found " + myMatcher.group());
-            counterOfPairs++;
+            System.out.print("I found " + myMatcher.group());
+            if(myMatcher.find()) {
+                System.out.println(" + " + myMatcher.group());
+                counterOfPairs++;
+            }
         }
         /* Show number of matches */
-        System.out.println(counterOfPairs + " pairs in total");
+        System.out.println("\n" + counterOfPairs + " pairs in total");
     }
 }
